1d0b2e38ea782898a69fe778a5560e0dd2ee7077
Rafa-afkdev <sirfucho@gmail.com>
2026-09-08 23:29:02 -0400

    feat: implement appointments page with Calendly integration and dynamic booking content

--- src/app/[locale]/NavbarMain.tsx
+++ src/app/[locale]/NavbarMain.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
 import {
-  Calendar,
   ChevronDown,
   GraduationCap,
   Heart,
@@ -130,11 +129,11 @@ export default function NavbarMain() {
 
   const servicesOptions = [
     { href: "/services", label: t("ourServices"), icon: HeartHandshake },
-    {
-      href: "/services/appointments",
-      label: t("appointments"),
-      icon: Calendar,
-    },
+    // {
+    //   href: "/services/appointments",
+    //   label: t("appointments"),
+    //   icon: Calendar,
+    // },
     { href: "/warehouse", label: t("warehouse"), icon: Package },
     { href: "/education", label: t("education"), icon: GraduationCap },
   ];
